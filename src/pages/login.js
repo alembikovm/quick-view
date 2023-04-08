1f9d3b747c9d89cf81bbdf23a8d1a58077a6ecc3
Marat Alembikov <alembikovm@gmail.com>
2023-04-08 18:48:04 +0300

    feat: Added responsive layout for login/sign up

--- src/pages/login.js
+++ src/pages/login.js
@@ -32,10 +32,10 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
+      <div className="flex w-0 md:w-3/5">
         <img src="/images/iphone-with-profile.jpg" alt="iPhone with quick-view app" />
       </div>
-      <div className="flex flex-col w-2/5">
+      <div className="flex flex-col w-5/5 md:w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
           <h1 className="flex justify-center w-full">
             <img src="/images/logo.png" alt="quick-view" className="mt-2 w-6/12 mb-4" />
